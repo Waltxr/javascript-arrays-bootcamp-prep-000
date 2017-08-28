bedd78a1f213c3a13ed4b281e3d4e5eeac805649
Adam Walter <adam.iverson.walter@gmail.com>
2017-08-28 17:22:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,8 +11,10 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array = [element, ...array]
-  return array
+  return array.unshift(element)
+
+  // array = [element, ...array]
+  // return array
 }
 
 function addElementToEndOfArray(array, element) {
